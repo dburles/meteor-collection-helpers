06da1c3c230469bfd303030a86d507b356201b4d
David Burles <dburles@gmail.com>
2013-12-31 17:26:10 +1100

    initial

---
+++ collection-helpers.js
@@ -0,0 +1,23 @@
+var constructor = Meteor.Collection;
+
+Meteor.Collection = function(name, options) {
+  if (typeof options === 'undefined')
+    options = {};
+  options.transform = function(doc) { return new Collection(doc); };
+  return constructor.call(this, name, options);
+};
+
+Collection = function(doc) { return _.extend(this, doc); };
+
+Meteor.Collection.prototype = Object.create(constructor.prototype);
+Meteor.Collection.prototype.helpers = function(helpers) {
+  _.each(helpers, function(helper, key) {
+    Collection.prototype[key] = helper;
+  });
+};
+
+for (var func in constructor) {
+  if (constructor.hasOwnProperty(func)) {
+    Meteor.Collection[func] = constructor[func];
+  }
+}
