9ba93a2af80e6fa64b03e12d0d7f8816523325bb
David Burles <dburles@gmail.com>
2014-01-02 10:29:32 +1100

    fix for helpers applying to all collections
    for #1

--- collection-helpers.js
+++ collection-helpers.js
@@ -1,18 +1,23 @@
 var constructor = Meteor.Collection;
+var Document = {};
 
 Meteor.Collection = function(name, options) {
+  var self = this;
   if (typeof options === 'undefined')
     options = {};
-  options.transform = function(doc) { return new Document(doc); };
+
+  self._name = name;
+  Document[self._name] = function(doc) { return _.extend(this, doc); };
+  
+  options.transform = function(doc) { return new Document[self._name](doc); };
   return constructor.call(this, name, options);
 };
 
-Document = function(doc) { return _.extend(this, doc); };
-
 Meteor.Collection.prototype = Object.create(constructor.prototype);
 Meteor.Collection.prototype.helpers = function(helpers) {
+  var self = this;
   _.each(helpers, function(helper, key) {
-    Document.prototype[key] = helper;
+    Document[self._name].prototype[key] = helper;
   });
 };
 
