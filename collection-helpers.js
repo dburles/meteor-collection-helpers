29dacd1e14474c44e777764a939372d81bbaa1e0
David Burles <dburles@gmail.com>
2014-01-14 17:48:46 +1100

    big refactor
    thanks @tmeasday !

--- collection-helpers.js
+++ collection-helpers.js
@@ -1,36 +1,11 @@
-var constructor = Meteor.Collection;
 var Document = {};
 
-Meteor.Collection = function(name, options) {
-  if (typeof options === 'undefined')
-    options = {};
-
-  Document[name] = function(doc) { return _.extend(this, doc); };
-  options.transform = function(doc) { return new Document[name](doc); };
-
-  return constructor.call(this, name, options);
-};
-
-Meteor.Collection.prototype = Object.create(constructor.prototype);
 Meteor.Collection.prototype.helpers = function(helpers) {
   var self = this;
+  if (! Document[self._name]) Document[self._name] = function(doc) { return _.extend(this, doc); };
   _.each(helpers, function(helper, key) {
     Document[self._name].prototype[key] = helper;
   });
+  if (! this._transform)
+    this._transform = function(doc) { return new Document[self._name](doc); };
 };
-
-for (var func in constructor) {
-  if (constructor.hasOwnProperty(func)) {
-    Meteor.Collection[func] = constructor[func];
-  }
-}
-
-if (typeof Meteor.users !== 'undefined') {
-  var User = function(doc) { return _.extend(this, doc); };
-  Meteor.users._transform = function(doc) { return new User(doc); };
-  Meteor.users.helpers = function(helpers) {
-    _.each(helpers, function(helper, key) {
-      User.prototype[key] = helper;
-    });
-  };
-}
