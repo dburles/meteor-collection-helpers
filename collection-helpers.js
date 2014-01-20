52c4c692421acea557fb329f43f65ed11b4832df
David Burles <dburles@gmail.com>
2014-01-20 22:28:35 +1100

    minor re-jig

--- collection-helpers.js
+++ collection-helpers.js
@@ -2,8 +2,8 @@ var Document = {};
 Meteor.Collection.prototype.helpers = function(helpers) {
   var self = this;
   if (! Document[self._name]) Document[self._name] = function(doc) { return _.extend(this, doc); };
+  if (! self._transform) self._transform = function(doc) { return new Document[self._name](doc); };
   _.each(helpers, function(helper, key) {
     Document[self._name].prototype[key] = helper;
   });
-  if (! self._transform) this._transform = function(doc) { return new Document[self._name](doc); };
 };
