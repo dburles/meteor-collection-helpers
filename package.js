31ed52a1ad8e341082aa2ace3d013abe1fc7705a
dcsan <dc@pikkle.com>
2014-05-11 14:13:37 -0700

    WIP adding some tests

--- package.js
+++ package.js
@@ -6,3 +6,14 @@ Package.on_use(function(api) {
   api.use(['underscore']);
   api.add_files('collection-helpers.js', ['client', 'server']);
 });
+
+Package.on_test(function (api) {
+	api.use([
+		'coffeescript',
+		'tinytest',
+		'test-helpers',
+	],
+	['client', 'server']
+	);
+	api.add_files(['collection-helpers-test.coffee'], ['server'] )
+});
