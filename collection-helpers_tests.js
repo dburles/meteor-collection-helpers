912be6d15a59b9ee4519bf73000be70dd141b655
David Burles <dburles@gmail.com>
2014-05-16 16:37:03 +1000

    added tests

---
+++ collection-helpers_tests.js
@@ -0,0 +1,51 @@
+Tinytest.add("collection-helpers - works", function(test) {
+  Books = new Meteor.Collection('books' + test.id);
+  Authors = new Meteor.Collection('authors' + test.id);
+
+  var author1 = Authors.insert({
+    firstName: 'Charles',
+    lastName: 'Darwin'
+  });
+
+  var author2 = Authors.insert({
+    firstName: 'Carl',
+    lastName: 'Sagan'
+  });
+
+  var book1 = Books.insert({
+    authorId: author1,
+    name: 'On the Origin of Species'
+  });
+
+  var book2 = Books.insert({
+    authorId: author2,
+    name: 'Contact'
+  });
+
+  Books.helpers({
+    author: function() {
+      return Authors.findOne(this.authorId);
+    }
+  });
+
+  Authors.helpers({
+    fullName: function() {
+      return this.firstName + ' ' + this.lastName;
+    },
+    books: function() {
+      return Books.find({ authorId: this._id });
+    }
+  });
+
+  var book = Books.findOne(book1);
+  var author = book.author();
+  test.equal(author.firstName, 'Charles');
+
+  book = Books.findOne(book2);
+  author = book.author();
+  test.equal(author.fullName(), 'Carl Sagan');
+
+  author = Authors.findOne(author1);
+  books = author.books();
+  test.equal(books.count(), 1);
+});
--- package.js
+++ package.js
@@ -4,5 +4,11 @@ Package.describe({
 
 Package.on_use(function(api) {
   api.use('underscore');
-  api.add_files('collection-helpers.js', ['client', 'server']);
+  api.add_files('collection-helpers.js');
+});
+
+Package.on_test(function(api) {
+  api.use('tinytest');
+  api.use('collection-helpers');
+  api.add_files('collection-helpers_tests.js');
 });
