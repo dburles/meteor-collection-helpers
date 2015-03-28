ba3708429846252059462b1fe44e06555253557f
David Burles <dburles@gmail.com>
2015-03-28 11:44:47 +1100

    slight refactor

--- collection-helpers.js
+++ collection-helpers.js
@@ -1,18 +1,18 @@
-var Document = {};
 Mongo.Collection.prototype.helpers = function(helpers) {
   var self = this;
 
-  if (self._transform && ! self._hasCollectionHelpers)
+  if (self._transform && ! self._helpers)
     throw new Meteor.Error("Can't apply helpers to '" +
       self._name + "' a transform function already exists!");
 
-  if (! self._hasCollectionHelpers) {
-    Document[self._name] = function(doc) { return _.extend(this, doc); };
-    self._transform = function(doc) { return new Document[self._name](doc); };
-    self._hasCollectionHelpers = true;
+  if (! self._helpers) {
+    self._helpers = function Document(doc) { return _.extend(this, doc); };
+    self._transform = function(doc) {
+      return new self._helpers(doc);
+    };
   }
-  
+
   _.each(helpers, function(helper, key) {
-    Document[self._name].prototype[key] = helper;
+    self._helpers.prototype[key] = helper;
   });
 };
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "dburles:collection-helpers",
   summary: "Transform your collections with helpers that you define",
-  version: "1.0.2",
+  version: "1.0.3",
   git: "https://github.com/dburles/meteor-collection-helpers.git"
 });
 
