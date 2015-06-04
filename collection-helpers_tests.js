7ee366c403914ff905a68e3b718b2d944db94a90
Stefan Bergh <it.stefan.bergh@gmail.com>
2015-06-04 12:52:13 +0200

    added tests

--- collection-helpers_tests.js
+++ collection-helpers_tests.js
@@ -14,18 +14,52 @@ Tinytest.add("works", function(test) {
 
   var book1 = Books.insert({
     authorId: author1,
-    name: 'On the Origin of Species'
+    name: 'On the Origin of Species',
+    chapters: [
+      {
+          pages: ["page1"]
+      },
+      {
+          pages: ["page1", "page2"]
+      },
+      {
+          pages: ["page1", "page2", "page3"]
+      }
+    ],
+    arrayWithComplexObjects: [
+      { nest: { helper: { insideArrays: { number: 1 } } } },
+      { nest: { helper: { insideArrays: { number: 2 } } } }
+    ],
+    nested: { property: { on: { book: { number: 54321 } } } }
   });
 
   var book2 = Books.insert({
     authorId: author2,
-    name: 'Contact'
+    name: 'Contact',
+    chapters: [],
+    arrayWithComplexObjects: [],
+    nested: { property: { on: { book: { number: 2 } } } }
   });
 
   Books.helpers({
-    author: function() {
-      return Authors.findOne(this.authorId);
-    }
+      author: function () {
+          return Authors.findOne(this.authorId);
+      }
+  });
+
+  Books.helpers({
+      "nested.property.on.book.getNumber": function () {
+          return this.number;
+      },
+      "chapters.$.numberOfPages": function () {
+          return this.pages.length;
+      },
+      "arrayWithComplexObjects.$.nest.helper.insideArrays.getNumber": function () {
+          return this.number;
+      },
+      "arrayWithComplexObjects.$.nest.helper.getNext": function () {
+          return this;
+      }
   });
 
   // We should be able to apply more if we wish
@@ -54,21 +88,29 @@ Tinytest.add("works", function(test) {
   author = Authors.findOne(author1);
   books = author.books();
   test.equal(books.count(), 1);
+
+  // test nested helpers
+  book = Books.findOne(book1);
+  test.equal(book.nested.property.on.book.getNumber(), 54321);
+  test.equal(book.chapters[1].numberOfPages(), 2);
+  test.equal(book.arrayWithComplexObjects[0].nest.helper.insideArrays.getNumber(), 1);
+  test.equal(book.arrayWithComplexObjects[0].nest.helper.getNext().insideArrays.number, 1);
 });
 
-Tinytest.add("throw error if transform function already exists", function(test) {
-  Author = function(doc) { return _.extend(this, doc); };
+Tinytest.add("throw error if transform function already exists", function (test) {
+    Author = function (doc) { return _.extend(this, doc); };
 
-  Author.prototype.fullName = 'Charles Darwin';
+    Author.prototype.fullName = 'Charles Darwin';
 
-  Authors = new Meteor.Collection('authors' + test.id, {
-    transform: function(doc) { return new Author(doc); }});
-
-  test.throws(function() {
-    Authors.helpers({
-      fullName: function() {
-        return this.firstName + ' ' + this.lastName;
-      }
+    Authors = new Meteor.Collection('authors' + test.id, {
+        transform: function (doc) { return new Author(doc); }
     });
-  });
+
+    test.throws(function () {
+        Authors.helpers({
+            fullName: function () {
+                return this.firstName + ' ' + this.lastName;
+            }
+        });
+    });
 });
