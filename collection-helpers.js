61327be4a56a4fa8db1c214abd71088224eb8fd1
Kostya Koostyushko <kostanos@gmail.com>
2015-09-12 20:44:24 -0400

    Added support for multiple _transform functions in collection #46

--- collection-helpers.js
+++ collection-helpers.js
@@ -1,14 +1,11 @@
 Mongo.Collection.prototype.helpers = function(helpers) {
   var self = this;
 
-  if (self._transform && ! self._helpers)
-    throw new Meteor.Error("Can't apply helpers to '" +
-      self._name + "' a transform function already exists!");
-
   if (! self._helpers) {
     self._helpers = function Document(doc) { return _.extend(this, doc); };
+    orig_transform = self._transform ? self._transform : function(doc) {return doc; }
     self._transform = function(doc) {
-      return new self._helpers(doc);
+      return new self._helpers(orig_transform(doc));
     };
   }
 
