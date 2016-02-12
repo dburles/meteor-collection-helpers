1a67bc921195099d4dd3ddefe7ce0dc9ef78d9a8
Geordie J <geojay@gmail.com>
2016-02-12 17:45:28 +0100

    1.1.0 Works with TAPi18n and other document transform functions! (inc. tests)

--- collection-helpers.js
+++ collection-helpers.js
@@ -1,19 +1,47 @@
 Mongo.Collection.prototype.helpers = function(helpers) {
-  var self = this;
+    let self = this;
 
-  if (!self._helpers) {
-    self._helpers = function Document(doc) {
-      return _.extend(this, doc);
-    };
+    if (!self._helpers) {
+        let OriginalDocument = class OriginalDocument {};
 
-    var originalTransform = self._transform || function(doc) {return doc};
-    self._transform = function(doc) {
-      doc = originalTransform.call(this, doc);
-      return new self._helpers(doc);
-    };
-  }
+        if (self._transform) {
+            let transformedDoc = createDummyTransformedDocument.call(self);
+            OriginalDocument = transformedDoc.__proto__.constructor;
+            if (OriginalDocument === Object) {
+                OriginalDocument = self._transform;
+            }
+        }
 
-  _.each(helpers, function(helper, key) {
-    self._helpers.prototype[key] = helper;
-  });
+        self._helpers = class DocumentWithHelpers extends OriginalDocument {
+            constructor(doc) {
+                super(doc);
+                return _.extend(this, doc);
+            }
+        }
+
+        self._transform = function(doc) {
+            return new self._helpers(doc);
+        };
+    }
+
+    let transformedDoc = createDummyTransformedDocument.call(self);
+    _.each(helpers, function(helper, key) {
+        if (transformedDoc[key] === undefined) {
+            self._helpers.prototype[key] = helper;
+        } else {
+            throw new Meteor.Error(`A helper called ${key} already exists on ${self}`)
+        }
+    });
 };
+
+
+function createDummyTransformedDocument() {
+    try {
+        return this._transform({_id: 'fakeDocument'});
+    } catch(e) {
+        throw new Meteor.Error(`Error while adding helpers to your Collection`,
+            `This only happens when your Collection has an existing transform
+             that accesses non-initialised state internally and then you try to
+             add helpers via Collection.helpers({}).`)
+    }
+}
--- package.js
+++ package.js
@@ -1,25 +1,27 @@
 Package.describe({
-  name: "dburles:collection-helpers",
-  summary: "Transform your collections with helpers that you define",
-  version: "1.0.5",
-  documentation: "README.md",
-  git: "https://github.com/dburles/meteor-collection-helpers.git",
+    name: "ephemer:collection-helpers",
+    summary: "Transform Collections with self-defined helpers. Works with TAPi18n & other self-defined transforms!",
+    version: "1.1.0",
+    documentation: "README.md",
+    git: "https://github.com/ephemer/meteor-collection-helpers.git",
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.0.2');
-  api.use([
-    'underscore',
-    'mongo']);
+    api.versionsFrom('1.2.0.2');
+    api.use([
+        'ecmascript',
+        'underscore',
+        'mongo']);
 
-  api.addFiles('collection-helpers.js');
+    api.addFiles('collection-helpers.js');
 });
 
 Package.onTest(function(api) {
-  api.use([
-    'tinytest',
-    'underscore',
-    'mongo',
-    'dburles:collection-helpers']);
-  api.addFiles('collection-helpers_tests.js');
+    api.use([
+        'ecmascript',
+        'tinytest',
+        'underscore',
+        'mongo',
+        'ephemer:collection-helpers']);
+    api.addFiles('collection-helpers_tests.js');
 });
