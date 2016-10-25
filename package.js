6603180006d78419322a42871b9e60765207f45b
David Burles <dburles@gmail.com>
2016-10-26 09:35:12 +1100

    update versionsFrom

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: "dburles:collection-helpers",
   summary: "Transform your collections with helpers that you define",
-  version: "1.0.4",
+  version: "1.1.0",
   documentation: "README.md",
   git: "https://github.com/dburles/meteor-collection-helpers.git",
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.0.2');
+  api.versionsFrom('1.4.2');
   api.use([
     'underscore',
     'mongo']);
@@ -21,5 +21,6 @@ Package.onTest(function(api) {
     'underscore',
     'mongo',
     'dburles:collection-helpers']);
+
   api.addFiles('collection-helpers_tests.js');
 });
